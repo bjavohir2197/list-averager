3d0ca417cd10b43e820c0fb1d975b374c48b0f94
bjavohir2197 <bjavohir2197@gmail.com>
2026-04-18 15:24:10 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,9 @@
+def ortacha_qiymat(ro'yhat):
+    return sum(ro'yhat) / len(ro'yhat)
+```
+
+```python
+def otracha_qiymat(ro'yhat):
+    if len(ro'yhat) == 0:
+        return None
+    return sum(ro'yhat) / len(ro'yhat)
